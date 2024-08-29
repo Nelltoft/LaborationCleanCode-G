f2e42d7955f9cb0259b64d63472c9366836ee2a8
Nelltoft <nelltoft@hotmail.com>
2024-08-29 10:17:33 +0200

    Mocked playerdata so i can test the method that returns the highscore

---
+++ LaborationCleanCode-G.Infrastructure/Repositories/MockPlayerDataRepository.cs
@@ -0,0 +1,41 @@
+﻿using LaborationCleanCode_G.Core.Interfaces;
+using LaborationCleanCode_G.Core.Models;
+
+namespace LaborationCleanCode_G.Infrastructure.Repositories;
+
+public class MockPlayerDataRepository : IPlayerDataRepository
+{
+    public void AddPlayer(IPlayerData playerData)
+    {
+        StreamWriter output = new("mockhighscore.txt", append: true);
+        output.WriteLine($"{playerData.Name}#&#{playerData.TotalGuesses}");
+        output.Close();
+    }
+
+    public ICollection<PlayerData> GetAllPlayers()
+    {
+        StreamReader response = new("mockhighscore.txt");
+        List<PlayerData> playerList = new();
+        string line;
+
+        while ((line = response.ReadLine()!) is not null)
+        {
+            string[] playerNameAndGuesses = line.Split(new string[] { "#&#" }, StringSplitOptions.None);
+            string name = playerNameAndGuesses[0];
+            int guesses = Convert.ToInt32(playerNameAndGuesses[1]);
+            PlayerData player = new(name, guesses);
+            int index = playerList.IndexOf(player);
+            if (index < 0)
+            {
+                playerList.Add(player);
+            }
+            else
+            {
+                playerList[index].Update(guesses);
+            }
+        }
+        response.Close();
+
+        return playerList.OrderBy(p => p.Average()).ToList();
+    }
+}
---
+++ LaborationCleanCode-G.Tests/Repositories/MockPlayerDataRepositoryTests.cs
@@ -0,0 +1,31 @@
+﻿using LaborationCleanCode_G.Core.Interfaces;
+using LaborationCleanCode_G.Core.Models;
+
+namespace LaborationCleanCode_G.Infrastructure.Repositories.Tests
+{
+    [TestClass()]
+    public class MockPlayerDataRepositoryTests
+    {
+        IPlayerDataRepository _sut;
+
+        [TestInitialize]
+        public void Initialize() 
+        {
+            _sut = new MockPlayerDataRepository();
+        }
+
+        [TestMethod()]
+        public void GetAllPlayers_TestThatItReturnsAListOfPlayerData()
+        {
+            //assign
+            var player = new PlayerData("Björn", 3);
+            _sut.AddPlayer(player);
+
+            //act
+            var result = _sut.GetAllPlayers();
+
+            //assert
+            Assert.IsInstanceOfType<List<PlayerData>>(result);
+        }
+    }
+}
